7fa255b3deed8718b47930100bc67247e5b2c5f1
DhairyaAmin-24110105 <24110105@iitgn.ac.in>
2026-01-19 11:53:37 +0530

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(void)
+{
+    int N;
+    if (scanf("%d", &N) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+
+    long long (*A)[N] = malloc(N * sizeof *A);
+    long long (*B)[N] = malloc(N * sizeof *B);
+    long long (*C)[N] = malloc(N * sizeof *C);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = 1;
+            B[i][j] = 1;
+            C[i][j] = 0;
+        }
+    }
+    struct timespec start,end;
+    clock_gettime(CLOCK_MONOTONIC ,&start);
+    // Matrix multiplication
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                C[i][j] = A[i][k] * B[k][j];
+            }
+        }
+    }
+    clock_gettime(CLOCK_MONOTONIC,&end);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%lld ", C[i][j]);
+        }
+        printf("\n");
+    }
+    double elapsed=(end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec)/1e9;
+    printf("Time taken: %lf seconds\n",elapsed);
+
+    return 0;
+}
