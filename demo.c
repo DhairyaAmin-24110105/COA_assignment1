0064185e50eabcb1b17bffe217749eccc9b0d8fd
DhairyaAmin-24110105 <24110105@iitgn.ac.in>
2026-01-19 12:20:03 +0530

    Update demo.c

--- demo.c
+++ demo.c
@@ -11,14 +11,17 @@ int main(void)
         return 1;
     }
 
-    long long (*A)[N] = malloc(N * sizeof *A);
-    long long (*B)[N] = malloc(N * sizeof *B);
-    long long (*C)[N] = malloc(N * sizeof *C);
+    int **A = malloc(N * sizeof(int *));
+    int **B = malloc(N * sizeof(int *));
+    int **C = malloc(N * sizeof(int *));
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            A[i][j] = 1;
-            B[i][j] = 1;
+    for (int i=0; i<N; i++){
+        A[i] = malloc(N * sizeof(int));
+        B[i] = malloc(N * sizeof(int));
+        C[i] = malloc(N * sizeof(int));
+        for (int j=0; j<N; j++){
+            A[i][j] = 8;
+            B[i][j] = 9;
             C[i][j] = 0;
         }
     }
@@ -28,18 +31,12 @@ int main(void)
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             for (int k = 0; k < N; k++) {
-                C[i][j] = A[i][k] * B[k][j];
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
     clock_gettime(CLOCK_MONOTONIC,&end);
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%lld ", C[i][j]);
-        }
-        printf("\n");
-    }
-    double elapsed=(end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec)/1e9;
+    double elapsed=(end.tv_sec - start.tv_sec) + ((end.tv_nsec - start.tv_nsec)/1e9);
     printf("Time taken: %lf seconds\n",elapsed);
 
     return 0;
